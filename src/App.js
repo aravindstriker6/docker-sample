b29b23039f0825d27e0831218dddb69c25767ea2
Aravind Srinivasan <aravind61999@gmail.com>
2024-03-26 13:16:38 -0700

    initial project commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          An amazing startup idea!!! Let's do it!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          What is the idea?? yo??
+        </p>
+      
       </header>
     </div>
   );
